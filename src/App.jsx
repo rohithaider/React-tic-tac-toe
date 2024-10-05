19c5ec3436a0c60a20ec0eb0fcf5b9903a52409f
rohithaider <rohithaider@gmail.com>
2024-10-05 22:53:14 +0600

    Board and Square components created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
-export default function App() {
+
+function Square({value}) {
   return (
-    
-      <h1 className="text-red-700 font-bold text-4xl">Implemented Tailwind Successfully</h1>
-  
+    <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
+  return (
+    <>
+      <div>
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
+      </div>
+      <div>
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
+      </div>
+      <div>
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
+      </div>
+    </>
   );
 }
