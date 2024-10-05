74a1eb29ecc2a75d07dda2271d5538cfe3e0563a
rohithaider <rohithaider@gmail.com>
2024-10-05 21:16:41 +0600

    implemented tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     
-      <h1 className="text-red-700 font-bold text-4xl">Hello World</h1>
+      <h1 className="text-red-700 font-bold text-4xl">Implemented Tailwind Successfully</h1>
   
   );
 }
