9e7068c86f1c3832872ebf001eaf86a70b4571fc
rohithaider <rohithaider@gmail.com>
2024-10-06 13:19:57 +0600

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -11,43 +11,40 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-export default function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null));  //creating state for the parent and filling it up with array of 9 elements with null
-  const [xIsNext,setXIsNext]=useState(true);  //creating state for holding the value  for 'X' and 'O'
-
-  //calculating winner 
+function Board({xIsNext,squares, onPlay}) {
+  //calculating winner
   const winner = calculateWinner(squares);
   let status;
 
-  if(winner){
-    status = `Winner: ${winner}`
-  }else{
-    status = "Next Player "+(xIsNext?"X" : "O");
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = "Next Player " + (xIsNext ? "X" : "O");
   }
 
   function handleClick(i) {
     //checking if the square is blank
     //if square is not blank, then we won't continue or winner is found , we won't continue
-    if(squares[i] || calculateWinner(squares)){
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
 
-    const nextSquares = squares.slice();   //copy the array for not mutating the original squares
-    if(xIsNext){
-      nextSquares[i]="X";
-    }else{
-      nextSquares[i]="O";
+    const nextSquares = squares.slice(); //copy the array for not mutating the original squares
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
     }
-    setSquares(nextSquares);   //updating state and rendering 
-    setXIsNext(!xIsNext); //since it is a primitive value, we don't need to copy it
-  }
 
+    onPlay(nextSquares);
+    
+  }
   return (
     <>
       <div>{status}</div>
       <div className="flex">
         {/* /Passing arrow function to the onSquareClick so that it does not trigger. */}
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />   
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
         <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
@@ -65,21 +62,67 @@ export default function Board() {
   );
 }
 
-function calculateWinner(squares){
-  const lines=[
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6],
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]); //creating state for the parent and filling it up with array of 9 elements with null
+  const [xIsNext, setXIsNext] = useState(true); //creating state for holding the value  for 'X' and 'O'
+  const [currentMove,setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    setXIsNext(!xIsNext);
+    const nextHistory = [...history.slice(0,currentMove+1),nextSquares]
+    setHistory(nextHistory
+    )
+    setCurrentMove(nextHistory.length-1);
+  }
+  function jumpTo(move){
+    setCurrentMove(move);
+    setXIsNext(move %2 ===0);
+
+  }
+
+const moves = history.map((squares,move)=>{
+  let description;
+  if(move>0){
+    description = `Go to the move # ${move}`
+  }
+  else{
+    description = `Go to start the game`
+  }
+  return(
+    <li key={move}>
+      <button onClick={()=>jumpTo(move)}>{description}</button>
+    </li>
+  )
+})
+
+  return (
+    <div>
+      <div>
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div>
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
   ];
-  for (let i=0;i<lines.length;i++){
-    const[a,b,c] = lines[i];
-    if(squares[a] && squares[a] ===squares[b] && squares[a]===squares[c]){
-      return squares[a]
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
     }
   }
   return null;
